1f964a793cf6d3757acbe61b79c66ef286c0dc18
Cscharwitzl <clemens@scharwitzl.com>
2017-11-09 09:41:13 +0100

    get csv

--- d3.js
+++ d3.js
@@ -1,10 +1,31 @@
 
-let dataset = d3.csv("data.csv");
+let dataset;
 
-let svg = d3.select("figure")
-  .append("svg");
+d3.csv("data.csv", function(err, data) {
 
-svg.selectAll("rect")
-  .data(dataset)
-  .enter()
-  .append("rect");
+   dataset = data;
+   draw();
+
+});
+
+function draw(){
+  d3.select("figure")
+    .append("svg");
+
+    console.log(dataset);
+
+  d3.select("svg").selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("x", 20)
+    .attr("y", function(d,i){
+      return i * 50;
+    })
+    .attr("width", function(d) {
+      return d.Prozent * 10;
+    })
+    .attr("height", 25)
+    .attr("stroke", "black")
+    .attr("fill", "transparent");
+}
