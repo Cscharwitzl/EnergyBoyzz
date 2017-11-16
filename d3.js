22ca68b366439cff82d94b1a7b9fa4d9c3c5c8f7
Cscharwitzl <clemens@scharwitzl.com>
2017-11-16 08:59:05 +0100

    change d3

--- d3.js
+++ d3.js
@@ -14,18 +14,27 @@ function draw(){
 
     console.log(dataset);
 
-  d3.select("svg").selectAll("rect")
+  elements = d3.select("svg").selectAll("rect")
     .data(dataset)
     .enter()
     .append("rect")
-    .attr("x", 20)
-    .attr("y", function(d,i){
-      return i * 50;
-    })
+    .attr("y", 20)
     .attr("width", function(d) {
       return d.Prozent * 10;
     })
     .attr("height", 25)
     .attr("stroke", "black")
     .attr("fill", "transparent");
+
+  d3.select("svg").selectAll("rect")
+    .attr("x", function(d,i){
+      console.log(elements.data());
+      return d3.select("svg").selectAll("rect").data()[i-1] === undefined? 0:d3.select("svg").selectAll("rect").data()[i-1].Prozent*10;
+    });
+
+  d3.select("svg")
+    .attr("width", "100%")
+    .attr("viewBox", "0 0 1000 100");
+
+
 }
