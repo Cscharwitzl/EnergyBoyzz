3e720168d42475658c1bf7d60da95eae8c0d6d1e
nussi98 <nussbaumer.domi@gmail.com>
2017-12-14 09:17:06 +0100

    fixed everything - definitely finished

--- initForm.js
+++ initForm.js
@@ -7,9 +7,38 @@ for(let i = 0; i<consumption.length; i++){
     if(activeConsumption!=-1){
           consumption[activeConsumption].classList.remove("selected");
     }
-    consumption[i].classList.add("selected");
+    //consumption[i].classList.add("selected");
+    switch (i) {
+      case 0:
+        consumption[i].classList.add("selected");
+        break;
+      case 1:
+        consumption[i].classList.add("selected");
+        break;
+      case 2:
+        consumption[i].classList.add("selected");
+        break;
+    }
+
+    var active = consumption[i];
     activeConsumption = i;
     setup(consumption[i].dataset.kwh);
+
+    switch (activeConsumption) {
+      case 0:
+      console.log("1");
+        d3.select("Halo1").style("fill", colorScale(calcPreis()));
+        break;
+      case 1:
+      console.log("2");
+        d3.select("Halo2").style("fill", colorScale(calcPreis()));
+        break;
+      case 2:
+      console.log("3");
+        d3.select("Halo3").style("fill", colorScale(calcPreis()));
+        break;
+    }
+
     console.log(activeConsumption);
   })
 }
--- main.js
+++ main.js
@@ -1,35 +1,45 @@
-
-
 var resize = d3.drag()
         .on('drag', function () {
           d3.select(this)
-                    .attr('width', function (c, i) {
-                      var value = parseFloat(d3.event.dx);
-                      let anzahleElements = getVisible()-1;
+            .attr('width', function (c, i) {
+              var value = parseFloat(d3.event.dx);
+              let anzahleElements = getVisible()-1;
 
-                      elements.filter(function (d, i) {return d.Name !== c.Name;})
-                        .attr("width", function(d,i){
+              elements.filter(function (d, i) {return d.Name !== c.Name;})
+                .attr("width", function(d,i){
 
-                          if(anzahleElements == 0 && value < 0){
-                            return -1 * parseFloat(value) / (parseFloat(dataset.length)-1);
-                          }else{
-                            return parseFloat(this.attributes.width.value) - parseFloat(value)/parseFloat(anzahleElements) <0? 0:parseFloat(this.attributes.width.value) - parseFloat(value)/parseFloat(anzahleElements);
-                          }
-                        });
+                  if(anzahleElements == 0 && value < 0){
+                    return -1 * parseFloat(value) / (parseFloat(dataset.length)-1);
+                  }else{
+                    return parseFloat(this.attributes.width.value) - parseFloat(value)/parseFloat(anzahleElements) <0? 0:parseFloat(this.attributes.width.value) - parseFloat(value)/parseFloat(anzahleElements);
+                  }
+                });
 
-                        if(anzahleElements == 0){
-                          return parseFloat(this.attributes.width.value);
-                        }else {
-                            return  parseFloat(this.attributes.width.value) + value < 0? 0:parseFloat(this.attributes.width.value) + value;
-                        }
+                if(anzahleElements == 0){
+                  return parseFloat(this.attributes.width.value);
+                }else {
+                    return  parseFloat(this.attributes.width.value) + value < 0? 0:parseFloat(this.attributes.width.value) + value;
+                }
 
 
-                    });
+            });
 
             setX();
             calcPreis();
         });
 
+
+//color scale
+var colorScale = d3.scaleLinear()
+    .domain([0, 40,100])
+    .range(['#6BBC68', '#F8D800', '#EE2B47']);
+
+
+let dataset;
+let haushalt;
+let max;
+let elements;
+let labels;
 
 function calcPreis(){
 
@@ -42,6 +52,20 @@ function calcPreis(){
 
   d3.select(".preis")
     .text((Math.round(sum *100) /100)+"€ / Monat");
+
+    console.log((Math.round(sum *100) /100));
+    console.log(colorScale((Math.round(sum *100) /100)));
+
+
+    if(d3.select(".cons").classed("selected")){
+        d3.select(".Halo")
+          .style("fill", colorScale((Math.round(sum *100) /100)));
+    }
+
+    d3.select("figcaption")
+      .style("color", colorScale((Math.round(sum *100) /100)));
+
+    return Math.round(sum *100) / 100;
 }
 
 function getVisible() {
@@ -58,17 +82,8 @@ function getMax(){
   return erg+10;
 }
 
-
-let dataset;
-let haushalt;
-let max;
-let elements;
-let labels;
-
-
-
 function setX(){
-  d3.select("svg").selectAll("rect")
+  d3.select(".d3").select("svg").selectAll("rect")
     .attr("x", function(data, index){
       var x = 0;
       let width = this.attributes.width.value;
@@ -86,8 +101,7 @@ function setX(){
           })
           .classed("noshow", function(d){
               return ((width-this.getBBox().width)/2) < 0;
-          })
-          ;
+          });
 
       return x;
     });
@@ -96,53 +110,50 @@ function setX(){
 function setup(kwh){
   haushalt = parseFloat(kwh);
 
-  d3.select(".d3").html("");;
+  d3.select(".d3").html("");
 
-d3.csv("endverbrauch2016.csv", function(err, data) {
-   dataset = data;
-   max = getMax();
-   draw();
-});
+  d3.csv("endverbrauch2016.csv", function(err, data) {
+     dataset = data;
+     max = getMax();
+     draw();
+  });
 }
-
-
-
 
 function draw(){
 
-d3.select(".d3")
+  d3.select(".d3")
     .append("svg");
 
-g =   d3.select("svg").selectAll("g")
-    .data(dataset)
-    .enter()
-    .append("g");
-  elements =  g.append("rect")
-    .classed("resizingContainer", true)
-    .attr("y", 5)
-    .attr("width", function(d){
-      return parseFloat(d.Prozent/100);
-    })
-    .attr("height", 500)
-    .style("fill", "transparent")
-    .style("stroke", "#000")
-    .attr("data-preis", function(d){
-      //return d.Prozent/(max*100) * preis;
+    g = d3.select(".d3").select("svg").selectAll("g")
+          .data(dataset)
+          .enter()
+          .append("g");
+          elements =  g.append("rect")
+          .classed("resizingContainer", true)
+          .attr("y", 5)
+          .attr("width", function(d){
+            return parseFloat(d.Prozent/100);
+          })
+          .attr("height", 500)
+          //.style("fill", "transparent")
+          .style("stroke", "#000")
+          .attr("data-preis", function(d){
+            //return d.Prozent/(max*100) * preis;
       return d.Preisprokwh;
     })
     .call(resize);
 
-labels = g.append("text")
-      .text(function(d){
-        return d.Name;
-      })
-      .attr("y", 250)
-      .attr("fill", "black")
-      .attr("font-size","150px");
+  labels = g.append("text")
+            .text(function(d){
+              return d.Name;
+            })
+            .attr("y", 310)
+            .attr("fill", "black")
+            .attr("font-size","150px");
 
   setX();
 
-  d3.select("svg")
+  d3.select(".d3").select("svg")
     .attr("width", "100%")
     .attr("viewBox", "0 0 "+max+" "+520);
 
@@ -151,5 +162,6 @@ labels = g.append("text")
     .classed("preis", true);
 
   calcPreis();
+
 
 }
